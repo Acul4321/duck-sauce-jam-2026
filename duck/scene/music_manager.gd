7448a13286d7a47fbacc3849a4ac2a2726a9158f
Otto <danielottojackpetersen2@gmail.com>
2026-02-21 22:06:30 +0000

    created music manager, audio buses, and moved alien to shop folder

---
+++ duck/scene/music_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+class_name MusicManager
+
+@export var shop: AudioStreamPlayer
+@export var main: AudioStreamPlayer
+
+var current := "main"
+
+
+func toggle(scene := ""):
+	if !scene.is_empty(): current = scene
+	var tweenUp = create_tween()
+	var tweenDown = create_tween()
+	if current == "main":
+		tweenUp.tween_property(main, ^"volume_db", -80.0, 1.0)
+		tweenDown.tween_property(shop, ^"volume_db", 0.0, 1.0)
+		current = "shop"
+	else:
+		tweenUp.tween_property(shop, ^"volume_db", -80.0, 1.0)
+		tweenDown.tween_property(main, ^"volume_db", 0.0, 1.0)
+		current = "main"
