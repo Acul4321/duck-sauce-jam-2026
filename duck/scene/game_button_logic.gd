2381608da701b6c00759f8c6ab0beee1c7807851
Acul4321 <lucarogers666@gmail.com>
2026-02-21 20:04:56 +0000

    button skeleton

---
+++ duck/scene/game_button_logic.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var shop_button: Button = $ShopButton
+@onready var bin_icon: Button = $BinIcon
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
