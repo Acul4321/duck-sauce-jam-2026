4f02239ec40785df60fdcfa8e25de4e3a7d506dd
Acul4321 <lucarogers666@gmail.com>
2026-02-21 18:49:07 +0000

    money on loop

--- duck/scene/planet/orbit_movement.gd
+++ duck/scene/planet/orbit_movement.gd
@@ -2,8 +2,21 @@ extends PathFollow2D
 
 @export var speed : float = 100.0
 
+var previous_progress_ratio: float = 0.0
+
 func _process(delta):
 	progress += speed * delta
+	
+	# Detect when planet completes a full loop
+	var current_progress_ratio = progress_ratio
+	if current_progress_ratio < previous_progress_ratio:  # Wrapped around from ~1.0 to ~0.0
+		# Get the planet resource and add its reward
+		var planet_node = get_parent().get_parent()
+		if planet_node and "planet_resource" in planet_node:
+			var reward = planet_node.planet_resource.reward
+			Money.add_money(reward)
+	
+	previous_progress_ratio = current_progress_ratio
 	
 	# Apply lerped rotation to planet sprite from orbit_ghost
 	var orbit_ghost = get_parent().get_parent().get_node_or_null("orbitGhost")
---
+++ duck/script/money_label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Connect to the Money singleton's money_changed signal
+	Money.money_changed.connect(_on_money_changed)
+	
+	# Set initial text
+	text = str(int(Money.get_money()))
+
+
+# Called when money changes
+func _on_money_changed(new_amount: float) -> void:
+	text = str(int(new_amount))
