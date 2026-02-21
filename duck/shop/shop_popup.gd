e35ca6865c45ba492699ab36fbc71db7fefe1d44
Otto <danielottojackpetersen2@gmail.com>
2026-02-21 16:21:36 +0000

    planet costs

--- duck/shop/shop_popup.gd
+++ duck/shop/shop_popup.gd
@@ -10,6 +10,8 @@ var planet: PlanetClass:
 	set(value):
 		planet = value
 		planetImage.texture = planet.texture
+		planetName.text = planet.name
+		planetCost.text = str(planet.cost,"x")
 
 func _process(_delta: float) -> void:
 	if(Input.is_key_pressed(KEY_ESCAPE)):
