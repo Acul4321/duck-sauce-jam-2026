329ed23a958c65d755be86766f8044c3d8f67bb0
Otto <danielottojackpetersen2@gmail.com>
2026-02-22 13:36:40 +0000

    right click to orbit anti clockwise

--- duck/scene/planet/orbit_movement.gd
+++ duck/scene/planet/orbit_movement.gd
@@ -2,29 +2,32 @@ extends PathFollow2D
 
 @export var speed : float = 100.0
 
+var direction := 1
 var previous_progress_ratio: float = 0.0
 var money_popup_scene = preload("res://scene/money_popup.tscn")
 
 func _process(delta):
-	progress += speed * delta
-	
+	progress += speed * delta * direction
+
 	# Detect when planet completes a full loop
 	var current_progress_ratio = progress_ratio
+	if direction < 0:
+		current_progress_ratio = 1 / progress_ratio
 	if current_progress_ratio < previous_progress_ratio:  # Wrapped around from ~1.0 to ~0.0
 		# Get the planet resource and add its reward
 		var planet_node = get_parent().get_parent()
 		if planet_node and "planet_resource" in planet_node:
 			var reward = planet_node.planet_resource.reward
 			Money.add_money(reward)
-			
+
 			# Spawn money popup at planet position
 			var popup = money_popup_scene.instantiate()
 			popup.text = "+$" + str(int(reward))
 			popup.global_position = get_node("planet").global_position
 			get_tree().root.add_child(popup)
-	
+
 	previous_progress_ratio = current_progress_ratio
-	
+
 	# Apply lerped rotation to planet sprite from orbit_ghost
 	var orbit_ghost = get_parent().get_parent().get_node_or_null("orbitGhost")
 	if orbit_ghost:
