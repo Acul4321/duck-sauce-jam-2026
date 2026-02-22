d437b286d8247b72cd79d3916da87ad8bf026aa0
Acul4321 <Acul4321@users.noreply.github.com>
2026-02-22 15:19:25 +0000

    Deploying to gh-pages from @ Acul4321/duck-sauce-jam-2026@934c4f2781ef8fc628e326d1557f1f0558b9c197 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771773053|38607594';
+const CACHE_VERSION = '1771773548|48422422';
 /** @type {string} */
 const CACHE_PREFIX = 'duck-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
