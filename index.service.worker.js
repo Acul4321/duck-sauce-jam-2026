a1379c6a4509fcf661b52a4f147ddac567ce783f
scottish3dprinter <scottish3dprinter@users.noreply.github.com>
2026-02-22 14:01:00 +0000

    Deploying to gh-pages from @ Acul4321/duck-sauce-jam-2026@d32ff7e4ebe37df69e8df79d85b23e5c0129709f 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771764400|38225907';
+const CACHE_VERSION = '1771768843|38479034';
 /** @type {string} */
 const CACHE_PREFIX = 'duck-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
